6cddfbe318d7d1f89c0f213e2d07be957070ca1f
daniel-parra16 <danielestebanparraruiz22@gmail.com>
2026-04-17 23:22:18 -0500

    Fix: Completar los modelos de usuario y usuarioAuth

--- src/main/java/com/cognitio/gestion/models/Usuario.java
+++ src/main/java/com/cognitio/gestion/models/Usuario.java
@@ -1,28 +1,42 @@
 package com.cognitio.gestion.models;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "usuarios")
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
-@Table(name = "user")
-@Entity
+@Builder
 public class Usuario {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     @Column(nullable = false)
     private String first_name;
+
+    @Column(nullable = false)
     private String last_name;
+
+    @Column(nullable = false)
+    private LocalDateTime created_at;
+
+    @OneToMany(mappedBy = "usuario")
+    private UsuarioAuth auth;
 
 }
